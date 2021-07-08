bc992bbd7c24ef5428f81f604f0e0a750882cbc5
Raoul Koopman <raoul@fubo.tv>
2021-07-08 14:25:23 -0400

    Initial Commit

---
+++ HackerNewsApp/HackerNewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsAppApp.swift
+//  HackerNewsApp
+//
+//  Created by Raoul Koopman on 7/8/21.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

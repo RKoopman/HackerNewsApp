f532bc7d1e765161add7e1a194fd25dbc9d06ddf
Raoul Koopman <raoul.koopman@gmail.com>
2023-10-26 13:33:14 -0400

    update articles and saved lists

---
+++ HackerNewsApp/Views/Articles/Articles Main/ArticleCell.swift
@@ -0,0 +1,37 @@
+//
+//  ArticleCell.swift
+//  HackerNewsApp
+//
+//  Created by R K on 10/26/23.
+//
+
+import SwiftUI
+
+struct ArticleCell: View {
+    
+    @State var currentTheme: Theme = themes[0]
+
+    var points:Int
+    var title:String
+    var url:String? = " "
+    
+    var body: some View {
+        HStack {
+            Text(String(points))
+                .padding(.horizontal, 3)
+            VStack(alignment: .leading) {
+                Text(title)
+                    .foregroundColor(currentTheme.cellTitleTextColor)
+                Text(String(url!))
+                    .font(.system(size: 10))
+                    .lineLimit(1)
+                    .foregroundColor(currentTheme.bodyTextColorAlt)
+            }
+        }
+
+    }
+}
+
+//#Preview {
+//    ArticleCell(currentTheme: Theme, points: <#T##String#>, title: <#T##String#>, url: <#T##String#>)
+//}

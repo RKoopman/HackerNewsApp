868a03a83b234cc06f19a26505f22ab30bb6147b
Raoul Koopman <raoul.koopman@gmail.com>
2023-11-17 14:04:30 -0500

    add landing screen

---
+++ HackerNewsApp/Views/Articles/MainView.swift
@@ -0,0 +1,31 @@
+//
+//  MainView.swift
+//  HackerNewsApp
+//
+//  Created by R K on 11/17/23.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    
+    @State private var showLanding = true
+
+    var body: some View {
+        if showLanding == true {
+            HackerLandingView()
+                .onTapGesture {
+                    withAnimation {
+                        showLanding.toggle()
+                    }
+                }
+        } else {
+            HackerView()
+                .transition(.move(edge: .bottom))
+        }
+    }
+}
+
+#Preview {
+    MainView()
+}
